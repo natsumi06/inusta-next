2d134e297f5916f5ab39f2c6470b738b114fd093
natsumi06 <naijinmeihouteng@gmail.com>
2025-10-20 11:39:15 +0900

    feat: auth APIのGETメソッドを実装

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,5 @@
-import { handlers } from "./../../../../../auth";
-export const { GET, POST } = handlers;
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  return new NextResponse("hello world!");
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,14 +6,3 @@ export default NextAuth(authConfig).auth;
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.jpg$).*)"],
 };
-
-// export const middleware = (req: NextRequest) => {
-//   const { pathname } = req.nextUrl;
-
-//   if (pathname === "/login") {
-//     // 例: 未認証なら /home へリダイレクト
-//     return NextResponse.redirect(new URL("/home", req.url));
-//   }
-
-//   return NextResponse.next();
-// };
